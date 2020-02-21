6586bca916e6816c1f355c8aa015d4d49e9668c9
Christopher Leja <christopher.leja@gmail.com>
2020-02-21 03:10:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -61,7 +61,7 @@ class Person
   def call_friend(person_instance)
     self.happiness_change(3)
     person_instance.happiness_change(3)
-    "Hi, #{person_instance"
+    "Hi #{person_instance.name}!"
   end
 
   def limit(num)
